734da385a91ab5da47f9719be69963a8f86031f9
Huỳnh Tấn Thọ <htho379@gmail.com>
2022-11-23 16:50:38 +0700

    Colors and Routes

---
+++ lib/constants/colors.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+final blueSky = MaterialColor(0xFF3E96FF, blueSkySwatch);
+final red = MaterialColor(0xFFEB5757, redSwatch);
+final lime = MaterialColor(0xFF70DA74, limeSwatch);
+final purple = MaterialColor(0xFF9747FF, purpleSwatch);
+final background = MaterialColor(0xFFF3F7FF, backgroundSwatch);
+
+Map<int, Color> blueSkySwatch = {
+  50: const Color.fromRGBO(62, 150, 255, .1),
+  100: const Color.fromRGBO(62, 150, 255, .2),
+  200: const Color.fromRGBO(62, 150, 255, .3),
+  300: const Color.fromRGBO(62, 150, 255, .4),
+  400: const Color.fromRGBO(62, 150, 255, .5),
+  500: const Color.fromRGBO(62, 150, 255, .6),
+  600: const Color.fromRGBO(62, 150, 255, .7),
+  700: const Color.fromRGBO(62, 150, 255, .8),
+  800: const Color.fromRGBO(62, 150, 255, .9),
+  900: const Color.fromRGBO(62, 150, 255, 1),
+};
+
+Map<int, Color> redSwatch = {
+  50: const Color.fromRGBO(235, 87, 87, .1),
+  100: const Color.fromRGBO(235, 87, 87, .2),
+  200: const Color.fromRGBO(235, 87, 87, .3),
+  300: const Color.fromRGBO(235, 87, 87, .4),
+  400: const Color.fromRGBO(235, 87, 87, .5),
+  500: const Color.fromRGBO(235, 87, 87, .6),
+  600: const Color.fromRGBO(235, 87, 87, .7),
+  700: const Color.fromRGBO(235, 87, 87, .8),
+  800: const Color.fromRGBO(235, 87, 87, .9),
+  900: const Color.fromRGBO(235, 87, 87, 1),
+};
+
+Map<int, Color> limeSwatch = {
+  50: const Color.fromRGBO(112, 218, 116, .1),
+  100: const Color.fromRGBO(112, 218, 116, .2),
+  200: const Color.fromRGBO(112, 218, 116, .3),
+  300: const Color.fromRGBO(112, 218, 116, .4),
+  400: const Color.fromRGBO(112, 218, 116, .5),
+  500: const Color.fromRGBO(112, 218, 116, .6),
+  600: const Color.fromRGBO(112, 218, 116, .7),
+  700: const Color.fromRGBO(112, 218, 116, .8),
+  800: const Color.fromRGBO(112, 218, 116, .9),
+  900: const Color.fromRGBO(112, 218, 116, 1),
+};
+
+Map<int, Color> purpleSwatch = {
+  50: const Color.fromRGBO(151, 71, 255, .1),
+  100: const Color.fromRGBO(151, 71, 255, .2),
+  200: const Color.fromRGBO(151, 71, 255, .3),
+  300: const Color.fromRGBO(151, 71, 255, .4),
+  400: const Color.fromRGBO(151, 71, 255, .5),
+  500: const Color.fromRGBO(151, 71, 255, .6),
+  600: const Color.fromRGBO(151, 71, 255, .7),
+  700: const Color.fromRGBO(151, 71, 255, .8),
+  800: const Color.fromRGBO(151, 71, 255, .9),
+  900: const Color.fromRGBO(151, 71, 255, 1),
+};
+
+Map<int, Color> backgroundSwatch = {
+  50: const Color.fromRGBO(243, 247, 255, .1),
+  100: const Color.fromRGBO(243, 247, 255, .2),
+  200: const Color.fromRGBO(243, 247, 255, .3),
+  300: const Color.fromRGBO(243, 247, 255, .4),
+  400: const Color.fromRGBO(243, 247, 255, .5),
+  500: const Color.fromRGBO(243, 247, 255, .6),
+  600: const Color.fromRGBO(243, 247, 255, .7),
+  700: const Color.fromRGBO(243, 247, 255, .8),
+  800: const Color.fromRGBO(243, 247, 255, .9),
+  900: const Color.fromRGBO(243, 247, 255, 1),
+};
---
+++ lib/constants/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const login = '/login/';
+  static const register = '/register/';
+  static const forgotPassword = '/forgot_password/';
+  static const homepage = '/homepage/';
+}
