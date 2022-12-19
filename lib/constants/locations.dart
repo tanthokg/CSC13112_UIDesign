054526cff1c63779101b6a81e8df813d401184ba
Huỳnh Tấn Thọ <htho379@gmail.com>
2022-12-19 16:12:34 +0700

    Input Location in another screen

---
+++ lib/constants/locations.dart
@@ -0,0 +1,44 @@
+import 'package:uniride/entity/location.dart';
+
+const locations = [
+  Location(
+    'KTX Khu A, Ký túc xá ĐHQG-HCM',
+    'Linh Trung, Thủ Đức, TPHCM',
+    'kí túc xá',
+  ),
+  Location(
+    'KTX Khu B, Ký túc xá ĐHQG-HCM',
+    'Đông Hòa, Dĩ An, Bình Dương',
+    'kí túc xá',
+  ),
+  Location(
+    'KTX 135B Trần Hưng Đạo',
+    'Phường Cầu Ông Lãnh, Q1, TPHCM',
+    'ký túc xá, kí túc xá',
+  ),
+  Location(
+    'Trường Đại học Khoa học Tự nhiên, ĐHQG-HCM',
+    'Linh Trung, Thủ Đức, TPHCM',
+    'hcmus, đh khtn, cs2, vnuhcm',
+  ),
+  Location(
+    'Trường Đại học Khoa học Tự nhiên, ĐHQG-HCM',
+    '227 Nguyễn Văn Cừ, P4, Q5, TPHCM',
+    'hcmus, đh khtn, cs1, vnuhcm',
+  ),
+  Location(
+    'Trường Đại học Quốc tế, ĐHQG-HCM',
+    'Khu phố 6, Linh Trung, Thủ Đức, TPHCM',
+    'iu, hcmiu, đh qt, vnuhcm',
+  ),
+  Location(
+    'Trường Đại học Bách Khoa, ĐHQG-HCM',
+    '268 Lý Thường Kiệt, P14, Q10, TPHCM',
+    'ut, hcmut, đh bk, cs1, vnuhcm',
+  ),
+  Location(
+    'Trường Đại học Bách Khoa, ĐHQG-HCM',
+    'Khu phố Tân Lập, Đông Hòa, Dĩ An, Bình Dương',
+    'ut, hcmut, đh bk, cs2, ''vnuhcm',
+  ),
+];
---
+++ lib/entity/location.dart
@@ -0,0 +1,7 @@
+class Location {
+  final String name;
+  final String detail;
+  final String alias;
+
+  const Location(this.name, this.detail, this.alias);
+}
